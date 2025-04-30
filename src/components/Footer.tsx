95d48fcb6b9708db98ffa2d09387d204caf43632
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-04-30 20:36:57 +0300

    added footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,26 @@
-const Footer = () => {
-  return <footer> Hi, it's footer.</footer>;
-};
+import { FaLinkedin, FaGithub } from 'react-icons/fa6';
+import { HiOutlineMail } from 'react-icons/hi';
+
+const Footer = () => (
+  <footer>
+    <div>&copy; 2025 Blog</div>
+    <div>
+      Made by ❤️<em>Daniel</em>❤️
+    </div>
+    <div>
+      <a href='https://github.com/' target='_blank' rel='noopener noreferrer'>
+        <FaGithub />
+      </a>
+
+      <a href='https://linkedin.com/' target='_blank' rel='noopener noreferrer'>
+        <FaLinkedin />
+      </a>
+
+      <a href='mailto:your@email.com'>
+        <HiOutlineMail />
+      </a>
+    </div>
+  </footer>
+);
 
 export default Footer;
