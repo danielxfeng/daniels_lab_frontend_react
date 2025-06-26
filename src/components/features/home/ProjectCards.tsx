732dd697e47c6efd3ee057c0e54d8be64cd521e5
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-06-26 21:45:22 +0300

    fix: add margin-top to projects list for improved layout spacing

--- src/components/features/home/ProjectCards.tsx
+++ src/components/features/home/ProjectCards.tsx
@@ -34,7 +34,7 @@ const ProjectCards = ({ position }: { position: 'page' | 'div' }) => {
 
       <div
         data-role='projects-list'
-        className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'
+        className='mt-12 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'
       >
         {projects.map((project) => (
           <Link to={project.projectLink} key={project.id}>
