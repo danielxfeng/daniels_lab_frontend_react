61435720c4e9779597a42f9b494bedbdff7c212a
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-06-26 22:46:24 +0300

    refactor: enhance AboutMe component layout and content structure

--- src/components/features/home/AboutMe.tsx
+++ src/components/features/home/AboutMe.tsx
@@ -1,198 +1,175 @@
 import MotionH1 from '@/components/motion_components/MotionH1';
 import MotionSpan from '@/components/motion_components/MotionSpan';
+import { GlowingEffect } from '@/components/third_party/GlowingEffect';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import siteMeta from '@/constants/siteMeta';
 import rawTechStack from '@/constants/techStack.json';
 import { cn } from '@/lib/utils';
 
 const highlightText = 'text-foreground text-xl font-semibold px-1';
-const normalText = 'text-muted-foreground italic px-0.5';
+const normalText = 'text-foreground/65 px-1 italic';
 
 const techStack = rawTechStack;
 
 const AboutMe = ({ position }: { position: 'page' | 'div' }) => {
   return (
-    <section data-role='about-me-section' className='inner-container my-3'>
+    <section
+      data-role='about-me-section'
+      className='inner-container flex flex-col text-lg leading-relaxed lg:my-6'
+    >
       {/* h1 or h2 based on the position */}
       {position === 'div' ? <h2 className='my-6'>About Me</h2> : <MotionH1>About Me</MotionH1>}
 
-      <div data-role='about-me-content' className='flex flex-col gap-6 py-8'>
+      <div data-role='about-me-content' className='flex w-full flex-col items-center gap-6 py-8'>
         {/* Introduction */}
         <section
           data-role='about-me-introduction'
-          className='mb-8 flex flex-col items-center gap-6 lg:flex-row'
+          className='mb-8 flex w-full max-w-2xl flex-col gap-8 lg:flex-row lg:items-center lg:gap-12'
         >
-          <div>
-            <Avatar className='ring-offset-background ring-muted z-5 h-24 w-24 shadow-xl ring-2 ring-offset-2'>
-              <AvatarImage src={siteMeta.myAvatar} alt='@Daniel' />
-              <AvatarFallback>Daniel</AvatarFallback>{' '}
-            </Avatar>
-          </div>
-          <div className='flex h-24 flex-col justify-between'>
-            <span className='text-highlight flex h-1/2 items-center px-2 py-1 text-3xl font-bold text-shadow-md dark:text-blue-500'>
-              {siteMeta.me}
-            </span>
-            <MotionSpan
-              text="Let's build something Coooooool !"
-              className='text-muted-foreground px-2 py-1 font-semibold italic'
-              delay={0.06}
+          <Avatar className='ring-offset-background ring-muted z-5 h-40 w-40 shadow-xl ring-2 ring-offset-2'>
+            <AvatarImage src={siteMeta.myAvatar} alt='@Daniel' />
+            <AvatarFallback>Daniel</AvatarFallback>{' '}
+          </Avatar>
+
+          <div
+            data-role='about-me-introduction-content'
+            className='border-border relative w-full rounded-2xl border bg-white p-6 shadow-md dark:bg-neutral-950'
+          >
+            <GlowingEffect
+              spread={40}
+              glow={true}
+              disabled={false}
+              proximity={64}
+              inactiveZone={0.01}
             />
+            <p>
+              <span className={cn(highlightText, 'block lg:inline-block')}>Hey!</span>
+              <span className={normalText}> I am</span>
+              <MotionSpan
+                text={siteMeta.me}
+                className='text-highlight px-2 py-1 text-3xl font-bold text-shadow-md'
+                delay={0.08}
+              />
+            </p>
+            <p>
+              <span className={highlightText}>{'CS student  ·  Helsinki'}</span>
+            </p>
+            <p>
+              <span className={cn(normalText, 'pl-0')}>I focus on</span>
+              <span className={highlightText}>full stack</span>
+              <span className={normalText}>
+                development, both backend and frontend, and also enjoy
+              </span>
+              <span className={highlightText}>system programming</span>
+              <span className={normalText}>and building</span>
+              <span className={highlightText}>AI</span>
+              <span className={normalText}>toys.</span>
+            </p>
           </div>
         </section>
 
-        {/* Education */}
-        <section data-role='about-me-education'>
-          <p>
-            <span className={cn(highlightText, 'ml-0 pl-0')} aria-hidden={true}>
-              Hey there!
-            </span>
-            <span className={normalText} aria-hidden={true}>
-              I’m a
-            </span>
-            <span className={highlightText} aria-hidden={true}>
-              computer science student
-            </span>
-            <span className={normalText} aria-hidden={true}>
-              currently living in
-            </span>
-            <span className={highlightText} aria-hidden={true}>
-              Helsinki
-            </span>
-            <span className={normalText} aria-hidden={true}>
-              with my family.
-            </span>
-          </p>
-          <ul className='ml-8 list-disc'>
-            <li>
-              <span className={cn(normalText, 'text-foreground mr-1 text-sm')} aria-hidden={true}>
-                10.2024 - 12.2025:
-              </span>
-              <span className={normalText} aria-hidden={true}>
-                Studying in
-              </span>
-              <span className={highlightText} aria-hidden={true}>
-                Hive Helsinki
-              </span>
-            </li>
-            <li>
-              <span className={cn(normalText, 'text-foreground mr-1 text-sm')} aria-hidden={true}>
-                8.2023 - 12.2025:
-              </span>
-              <span className={normalText} aria-hidden={true}>
-                Studying in
-              </span>
-              <span className={highlightText} aria-hidden={true}>
-                HAMK
-              </span>
-            </li>
-          </ul>
-          <p className='mt-3 mb-1.5'>
-            <span className={cn(normalText, 'ml-0 pl-0')} aria-hidden={true}>
-              I'm naturally curious and enjoy learning every day.
-            </span>
-            <span className={highlightText} aria-hidden={true}>
-              curious
-            </span>
-            <span className={normalText} aria-hidden={true}>
-              and enjoy
-            </span>
-            <span className={highlightText} aria-hidden={true}>
-              learning
-            </span>
-            <span className={normalText} aria-hidden={true}>
-              every day.
-            </span>
-          </p>
-          <p className={cn(normalText, 'ml-0 pl-0')}>
-            I love the feeling of being a little better than I was yesterday.
-          </p>
-        </section>
+        <div data-role='about-me-detail' className='flex max-w-5xl flex-col gap-12 lg:flex-row'>
+          <div
+            data-role='about-me-detail-left'
+            className='flex w-full flex-1 flex-col justify-start gap-3 rounded-2xl bg-white p-6 shadow-md dark:bg-neutral-950'
+          >
+            {/* Education Hive */}
+            <section data-role='about-me-education-hive'>
+              <p className='my-1'>
+                <span className={cn(normalText, 'pl-0')}>I am now studying in</span>
+                <span className={highlightText}>Hive Helsinki</span>
+                <span className={cn(normalText, 'pl-0')}>, working on system-level</span>
+                <span className={highlightText}>C/C++</span>
+                <span className={cn(normalText, 'pl-0')}>projects.</span>
+              </p>
+              <p className='my-1'>
+                <span className={cn(normalText, 'pl-0')}>
+                  These projects pushed me to design, debug, and build robust software, which
+                  greatly improved my real-world programming skills.
+                </span>
+              </p>
+            </section>
 
-        {/* Work */}
-        <section data-role='about-me-work'>
-          <p>
-            <span className={cn(normalText, 'ml-0 pl-0')} aria-hidden={true}>
-              Before August 2023, I worked in
-            </span>
-            <span className={highlightText} aria-hidden={true}>
-              corporate banking
-            </span>
-            <span className={normalText} aria-hidden={true}>
-              in
-            </span>
-            <span className={highlightText} aria-hidden={true}>
-              Shanghai
-            </span>
-            <span className={normalText} aria-hidden={true}>
-              , where I helped businesses manage their liquidity, acquire loans, handle hedging
-              strategies, and support M&A transactions.
-            </span>
-          </p>
-          <p>
-            <span className={cn(normalText, 'ml-0 pl-0')} aria-hidden={true}>
-              This experience helped me build strong
-            </span>
-            <span className={highlightText} aria-hidden={true}>
-              logical thinking
-            </span>
-            <span className={normalText} aria-hidden={true}>
-              for
-            </span>
-            <span className={highlightText} aria-hidden={true}>
-              breaking down
-            </span>
-            <span className={normalText} aria-hidden={true}>
-              complex problems (surprisingly useful when I switched to coding), and the ability to
-            </span>
-            <span className={highlightText} aria-hidden={true}>
-              work in a team
-            </span>
-            <span className={normalText} aria-hidden={true}>
-              .
-            </span>
-          </p>
-        </section>
+            {/* Education HAMK */}
+            <section data-role='about-me-education-hamk'>
+              <p className='my-1'>
+                <span className={cn(normalText, 'pl-0')}>I am also studying CS courses at</span>
+                <span className={highlightText}>HAMK</span>
+                <span className={cn(normalText, 'pl-0')}>.</span>
+              </p>
+            </section>
 
-        {/* Interest */}
-        <section data-role='about-me-interest'>
-          <p>
-            <span className='mb-2 block'>
-              <span className={normalText} aria-hidden={true}>
-                I love
-              </span>
-              <span className={highlightText} aria-hidden={true}>
-                traveling
-              </span>
-              <span className={normalText} aria-hidden={true}>
-                and spending time in
-              </span>
-              <span className={highlightText} aria-hidden={true}>
-                nature
-              </span>
-              <span className={normalText} aria-hidden={true}>
-                . Exploring scenic views and diverse cultures feels addictive to me.
-              </span>
-            </span>
-            <span className='block'>
-              <span className={normalText} aria-hidden={true}>
-                I also enjoy
-              </span>
-              <span className={highlightText} aria-hidden={true}>
-                taking photos
-              </span>
-              <span className={normalText} aria-hidden={true}>
-                , which helps me preserve those memories.
-              </span>
-            </span>
-          </p>
-        </section>
+            {/* Work */}
+            <section data-role='about-me-work'>
+              <p className='my-1'>
+                <span className={cn(normalText, 'pl-0')}>Before August 2023, I worked in</span>
+                <span className={highlightText}>corporate banking</span>
+                <span className={normalText}>in</span>
+                <span className={highlightText}>Shanghai</span>
+                <span className={cn(normalText, 'pl-0')}>.</span>
+              </p>
+              <p className='my-1'>
+                <span className={cn(normalText, 'pl-0')}>
+                  This experience helped me build strong
+                </span>
+                <span className={highlightText}>logical thinking</span>
+                <span className={normalText}>for</span>
+                <span className={highlightText}>breaking down</span>
+                <span className={normalText}>complex problems, and the ability to</span>
+                <span className={highlightText}>work in a team</span>
+                <span className={normalText}>.</span>
+              </p>
+            </section>
+          </div>
+
+          <div className='bg-muted hidden w-px lg:flex' />
+
+          <div
+            data-role='about-me-detail-right'
+            className='flex w-full flex-1 flex-col justify-start gap-6 rounded-2xl bg-white p-6 shadow-md dark:bg-neutral-950'
+          >
+            {/* Soft skill */}
+            <section data-role='about-me-soft-skill'>
+              <p className='my-1'>
+                <span className={cn(normalText, 'pl-0')}>I'm naturally</span>
+                <span className={highlightText}>curious</span>
+                <span className={normalText}>and enjoy</span>
+                <span className={highlightText}>learning</span>
+                <span className={normalText}>every day.</span>
+              </p>
+              <p className='my-1'>
+                <span className={cn(normalText, 'pl-0')}>
+                  I love the feeling of being a little better than I was yesterday.
+                </span>
+              </p>
+            </section>
+            <section data-role='about-me-soft-interest'>
+              <p className='my-1'>
+                <span className={normalText}>I love</span>
+                <span className={highlightText}>traveling</span>
+                <span className={normalText}>and spending time in</span>
+                <span className={highlightText}>nature</span>
+                <span className={normalText}>
+                  . Exploring scenic views and diverse cultures feels addictive to me.
+                </span>
+              </p>
+              <p>
+                <span className='block'>
+                  <span className={normalText}>I also enjoy</span>
+                  <span className={highlightText}>taking photos</span>
+                  <span className={normalText}>, which helps me preserve those memories.</span>
+                </span>
+              </p>
+            </section>
+          </div>
+        </div>
 
         {/* Tech Stacks */}
         <section
           data-role='about-me-tech'
-          className='flex flex-col gap-3 lg:flex-row lg:items-center lg:justify-around'
+          className='flex w-full flex-col gap-3 lg:flex-row lg:items-center lg:justify-around'
         >
-          <div data-role='about-me-tech-stack' className='flex flex-col gap-4 lg:gap-2 mt-10'>
+          <div data-role='about-me-tech-stack' className='mt-10 flex flex-col gap-4 lg:gap-2'>
             {techStack.map((item) => (
               <div key={item.title} className='mb-2 flex items-center gap-2'>
                 <div className='text-muted-foreground font-semibold italic'>
